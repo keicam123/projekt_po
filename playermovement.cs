c280111b27f9df23a5027d7974f3ec431d5c18c4
keicam123 <80455702+keicam123@users.noreply.github.com>
2021-03-24 18:08:25 +0100

    Add files via upload

---
+++ playermovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// script added for testing and learning puproses, will not be a part of final project
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    void start()
+    {
+
+    }
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
